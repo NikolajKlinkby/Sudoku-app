e4b76ef47d6020ab1b278efc707923e4a9141e0e
NikolajKlinkby <nikolaj.klinkby@gmail.com>
2022-05-15 15:18:49 +0200

    'Lucas joins the project'

--- Sudoku/src/main.cpp
+++ Sudoku/src/main.cpp
@@ -1,8 +1,7 @@
-#include <fstream>
-#include <iostream>
-#include <filesystem>
-#include <string>
 #include <cstdio>
+
+#include "SudokuLogic.h"
+
 #include <imgui.h>
 #include "imgui_impl_sdl.h"
 #include "imgui_impl_opengl3.h"
@@ -11,8 +10,6 @@
     #include <SDL_opengles2.h>
 #else
     #include <SDL_opengl.h>
-#include <cstring>
-
 #endif
 
 
@@ -49,18 +46,21 @@ int main() {
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
     #endif
 
-    // Create window with graphics context
+    // Setting attributes for the OpenGL SDL2. Doc: https://documentation.help/SDL/sdlglattr.html
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
     SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
     SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+
+    // Create window with graphics context
     SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-    SDL_Window* window = SDL_CreateWindow("Application name", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
+    SDL_Window* window = SDL_CreateWindow("Sudoku test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, gl_context);
     SDL_GL_SetSwapInterval(1); // Enable vsync
 
     // Setup Dear ImGui context
-    IMGUI_CHECKVERSION();
+    IMGUI_CHECKVERSION();//@TODO what is this???
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
 
@@ -71,24 +71,11 @@ int main() {
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init(glsl_version);
 
-    //State
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    //Color
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     //MAIN loop
     bool done = false;
-    std::string print;
-    std::ofstream text_depos_write;
-    std::ifstream text_depos_read;
-    std::string text_depos = ".text_depos";
-
-    text_depos_write.open (text_depos,std::ios::app);
-    text_depos_write.close();
-    std::filesystem::remove(text_depos);
-    text_depos_write.open (text_depos,std::ios::app);
-    text_depos_write.close();
-
     while (!done){
         // Start an instance of SDL
         SDL_Event event;
@@ -108,54 +95,12 @@ int main() {
 
         //Making a window
         bool tool_active = true;
-        const char *win_name = "Text writer";
-        ImGui::Begin(win_name,&tool_active,ImGuiWindowFlags_NoCollapse|ImGuiWindowFlags_MenuBar);
-
-        //Making a menu bar for the window
-        if (ImGui::BeginMenuBar())
-        {
-            if (ImGui::BeginMenu("Tools"))
-            {
-                if (ImGui::MenuItem("Quit", "shortcut")) {
-
-                    //Need to not create a segmentation fault
-
-                    // Perform cleanup
-                    ImGui_ImplOpenGL3_Shutdown();
-                    ImGui_ImplSDL2_Shutdown();
-                    ImGui::DestroyContext();
-
-                    SDL_GL_DeleteContext(gl_context);
-                    SDL_DestroyWindow(window);
-                    SDL_Quit();}
-
-                ImGui::EndMenu();
-            }
-            ImGui::EndMenuBar();
-        }
+        const char *win_name = "Grid";
+        ImGui::Begin(win_name,&tool_active,ImGuiWindowFlags_NoCollapse|ImGuiWindowFlags_NoMove|ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_NoResize);
 
 
-        //Creating a button with some text on it and above it
-        char buf[10];
-        ImGui::InputTextMultiline("Line",buf, IM_ARRAYSIZE(buf));
-
-        if (ImGui::Button("Write line")){
-            text_depos_write.open (text_depos,std::ios::app);
-            text_depos_write << buf << "\n";
-            text_depos_write.close();
-        }
-
-
-        //Creating a scrolling text section
-        ImGui::BeginChild("Scrolling");
-        text_depos_read.open (text_depos,std::ios::in);
-        while(!text_depos_read.eof()){
-            std::getline(text_depos_read, print);
-            ImGui::Text("%s", print.c_str());
-        }
-        text_depos_read.close();
-        ImGui::EndChild();
         ImGui::End();
+
 
         // Rendering
         ImGui::Render();
