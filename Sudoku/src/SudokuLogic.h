2eaea861a66ca77ae50421b1e65fbce2d5df0fc7
NikolajKlinkby <nikolaj.klinkby@gmail.com>
2022-05-16 21:08:49 +0200

    'Sudoku Logic: added simple solving logic'

--- Sudoku/src/SudokuLogic.h
+++ Sudoku/src/SudokuLogic.h
@@ -3,20 +3,47 @@
 class Sudoku  // The class
 {
 private:
-
     /*-- Private attributes --*/
     int rnd_number_list[9]{};
+
+    int annotation_solver_sudoku_array[9][9][9]{};
+    int solver_sudoku_array[9][9]{};
+
+    int solver_row_array[9];
+    int solver_col_array[9];
+    int solver_square_array[3][3];
+
+    /*-- private member functions --*/
+
+    void set_solver_col_to_current(int i);
+    void set_solver_row_to_current(int i);
+    void set_solver_square_to_current(int row,int col);
+
+    //Solver logic
+    void set_annotations();
+    bool check_annotation();
+    bool hidden_singles();
+
 
 public: // Access specifier for class
 
     /*-- Public attributes --*/
     int sudoku_array[9][9]{};
+    int solved_sudoku_array[9][9]{};
+
+    int row_array[9];
+    int col_array[9];
+    int square_array[3][3];
 
     /*-- Constructor --*/
     void Construct_Sudoku();
 
     /* -- Member Functions --*/
     void initialize_array();
+
+    void set_col_to_current(int i);
+    void set_row_to_current(int i);
+    void set_square_to_current(int row,int col);
 
     void set_rnd_number_list();
 
@@ -29,4 +56,6 @@ public: // Access specifier for class
     void print_sudoku();
 
 };
+
+
 #endif //SUDOKULOGIC_H
